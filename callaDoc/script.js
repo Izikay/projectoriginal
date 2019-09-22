c26401eb793b0a7c7fe4cd80941c399cf9220ac1
Izikay <gbadegesinisaac@gmail.com>
2019-09-22 00:48:36 -0700

    first commit

---
+++ callaDoc/script.js
@@ -0,0 +1,9 @@
+var plus = document.getElementsById('plus');
+var remain = document.getElementsById('remain');
+var faqs_q = document.getElementsById('faqs-q');
+
+
+function expand(){
+    remain.style.display = ('block');
+    plus.textContent = "-";
+}
